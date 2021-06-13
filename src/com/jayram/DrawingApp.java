afa482c6f7948fffb2e443d16091a0e23d410775
JayramNanoskar <jayram.n978@gmail.com>
2021-06-14 00:23:23 +0530

    initializing Collections

--- src/com/jayram/DrawingApp.java
+++ src/com/jayram/DrawingApp.java
@@ -11,10 +11,8 @@ public class DrawingApp {
 	public static void main(String[] args) {
 		//Using ClassPathXmlApplicationContext because going to use xml file and xml file will contain blueprint for beans to instantiate
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		Triangle triangle = (Triangle)context.getBean("triangle-name"); //Getting required bean from Spring containers ApplicationContext
+		Triangle triangle = (Triangle)context.getBean("triangle"); //Getting required bean from Spring containers ApplicationContext
 		triangle.draw(); 
-		Triangle triangle1 = (Triangle)context.getBean("triangle-alias");
-		triangle1.draw();
 	}
 
 }
