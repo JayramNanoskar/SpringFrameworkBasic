5668beb461ece87404b3f6c0344068d65c5a7f0f
JayramNanoskar <jayram.n978@gmail.com>
2021-06-13 19:02:40 +0530

    using the Bean Factory for object instantiation

--- src/com/jayram/DrawingApp.java
+++ src/com/jayram/DrawingApp.java
@@ -1,10 +1,16 @@
 package com.jayram;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle();
-		triangle.draw();
+		//Using XmlBeanFactory because going to use xml file and xml file will contain blueprint for beans to instantiate
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle)factory.getBean("triangle"); //Getting required bean from Spring containers Bean factory
+		triangle.draw(); 
 	}
 
 }
