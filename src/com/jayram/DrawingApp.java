a5749d5bf276e3ad576785152bca7656db54bc7a
JayramNanoskar <jayram.n978@gmail.com>
2021-06-13 20:17:36 +0530

    using the ApplicationContext for object instantiation

--- src/com/jayram/DrawingApp.java
+++ src/com/jayram/DrawingApp.java
@@ -2,14 +2,16 @@ package com.jayram;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		//Using XmlBeanFactory because going to use xml file and xml file will contain blueprint for beans to instantiate
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle = (Triangle)factory.getBean("triangle"); //Getting required bean from Spring containers Bean factory
+		//Using ClassPathXmlApplicationContext because going to use xml file and xml file will contain blueprint for beans to instantiate
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)context.getBean("triangle"); //Getting required bean from Spring containers ApplicationContext
 		triangle.draw(); 
 	}
 
